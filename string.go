2e18b986260f18d576c7451b4d82e50b244d6fd8
pygosuperman <pygosuperman@outlook.com>
2021-12-29 09:34:28 +0800

    string字符串相关操作

--- string.go
+++ string.go
@@ -30,7 +30,7 @@ func (r *Redis) Get(key string) (value string, err error) {
 }
 
 // 设置值，自定义过期时间
-func (r *Redis) SetExpire(key, value string, expire time.Duration) {
+func (r *Redis) SetExpire(key string, value interface{}, expire time.Duration) {
 	err := r.db.Set(context.Background(), key, value, expire).Err()
 	if err != nil {
 		r.log.Error("根据键设置值失败：", err)
@@ -38,6 +38,49 @@ func (r *Redis) SetExpire(key, value string, expire time.Duration) {
 }
 
 // 根据键设置值，过期时间默认为3小时
-func (r *Redis) Set(key, value string) {
+func (r *Redis) Set(key string, value interface{}) {
 	r.SetExpire(key, value, 3*60*60*time.Second)
+}
+
+// 同时获取多个键对应的值
+func (r *Redis) MGet(keys ...string) ([]interface{}, error) {
+	result, err := r.db.MGet(context.Background(), keys...).Result()
+	if err != nil {
+		r.log.Error("根据多个键同时获取值失败：", err)
+	}
+	return result, err
+}
+
+// 自增1
+func (r *Redis) Incr(key string) {
+	r.db.Incr(context.Background(), key)
+}
+
+// 自增n
+func (r *Redis) IncrBy(key string, n int64) {
+	r.db.IncrBy(context.Background(), key, n)
+}
+
+// 自减1
+func (r *Redis) Decr(key string) {
+	r.db.Decr(context.Background(), key)
+}
+
+// 自减n
+func (r *Redis) DecrBy(key string, n int64) {
+	r.db.DecrBy(context.Background(), key, n)
+}
+
+// 追加字符串
+func (r *Redis) Append(key string, value string) {
+	r.db.Append(context.Background(), key, value)
+}
+
+// 截取字符串
+func (r *Redis) Substr(key string, start, end int) (string, error) {
+	result, err := r.db.Do(context.Background(), "substr", key, start, end-1).Result()
+	if err != nil {
+		r.log.Error("截取字符串失败：", err)
+	}
+	return result.(string), err
 }
